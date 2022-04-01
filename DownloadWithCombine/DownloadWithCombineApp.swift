e76435dac17366e8eb6a4f79d026fedf8dae85f4
Vahtee Boo <vahtee.boo@gmail.com>
2022-04-01 15:41:03 +0300

    Initial Commit

---
+++ DownloadWithCombine/DownloadWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadWithCombineApp.swift
+//  DownloadWithCombine
+//
+//  Created by Vahtee Boo on 01.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct DownloadWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
